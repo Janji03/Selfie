f1a573489e30321f4a5e64d36141fa5ef4e38883
Lorenzo Casalini <lorenzocasalini7@gmail.com>
2024-11-14 20:05:14 +0100

    removed jwt field

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,7 @@
 const config = {
   port: process.env.PORT || 5000,
   dbURI: process.env.MONGO_URI ||
-    "mongodb+srv://root:root@selfie-mern.vukiuom.mongodb.net/?retryWrites=true&w=majority&appName=selfie-MERN",
-  jwtSecret: process.env.JWT_SECRET || "your_jwt_secret",
+    "mongodb+srv://root:root@selfie-mern.vukiuom.mongodb.net/?retryWrites=true&w=majority&appName=selfie-MERN"
 };
 
 export default config;
